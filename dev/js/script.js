8f8c989c298d3231c16fa554b84b5f455beae38e
Felipe Hanashiro <felipe.hanashiro@gauge.com.br>
2015-08-19 08:22:53 -0300

    scroll to

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,1 +1,15 @@
+var View = {
+	Nav: {
+		scrollTo: function(element){
+			$(window).scrollTo(element,{
+			  axis: 'y',
+			  duration: 800
+			});
+		}
+	}
+}
+
+
+
+
 $("nav.navbar-fixed-top").autoHidingNavbar();
