dc565c71e6e52d39d6a44b1d5da6a6d902a9c193
Felipe Hanashiro <felipe.hanashiro@gauge.com.br>
2015-09-08 09:24:23 -0300

    new Ingrid picture

--- banheiros/preview/js/script.js
+++ banheiros/preview/js/script.js
@@ -1,1 +1,14 @@
-var Media={Home:{closeModal:function(){$("#neve-play a").modal("hide")}}};$(document).ready(function(){$(".logo-container").on("click",function(){Media.Home.closeModal()})});
+var Media = {
+	Home: {
+		closeModal: function(){
+			$('#neve-play a').modal('hide');
+		}
+	}
+}
+
+
+$( document ).ready(function() {
+	$('.logo-container').on('click', function(){
+		Media.Home.closeModal();
+	})
+});
