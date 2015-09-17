64e1eea940e4ab05e71d86b958d8a0fe97efc77e
R.Frega <robertofrega@gmail.com>
2015-09-17 18:20:17 -0300

    Ajustes no Player

--- banheiros/preview/js/youtubeplayer.js
+++ banheiros/preview/js/youtubeplayer.js
@@ -1,53 +1,65 @@
 (function () {
 
-        var tag = document.createElement('script');
+    var tag = document.createElement('script');
 
-        tag.src = "https://www.youtube.com/iframe_api";
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-        // 3. E esta função é executada assim que a API fica pronta,
-        // inserindo o play na tag que tiver o id passado como primeiro parâmetro do construtor YT.Player
+    // 3. E esta função é executada assim que a API fica pronta,
+    // inserindo o play na tag que tiver o id passado como primeiro parâmetro do construtor YT.Player
 
-        window.onYouTubeIframeAPIReady = function () {
-            $(function () {
-                $('#video-modal > a').click(function(e){
-                    e.preventDefault();
-                    $('#video-modal').addClass('hidden');
-                    if (typeof window.myGaugePlayer == 'object' && typeof window.myGaugePlayer.stopVideo == 'function') {
-                        window.myGaugePlayer.pauseVideo()
-                    }
-                });
-                $('#neve-play').find('a').click(function (e) {
-                    e.preventDefault();
-                    $('#video-modal').removeClass('hidden');
-                    if (typeof window.myGaugePlayer == 'object' && typeof window.myGaugePlayer.playVideo == 'function') {
-                        window.myGaugePlayer.playVideo()
-                    } else {
-                        window.myGaugePlayer = new YT.Player('youtube-player', {
-                            height: '100%',
-                            width: '100%',
-                            videoId: 'XhVV5lUtlbc',
-                            events: {
-                                'onStateChange': Gauge.onPlayerStateChange,
-                                onReady: function(e){
-                                    e.target.playVideo();
-                                    var video_data = e.target.getVideoData();
-                                    var label = video_data.video_id + ':' + video_data.title;
-                                    dataLayer.push({
-                                        event: 'gauge.youtube',
-                                        action: 'start',
-                                        label: label
-                                    })
+    window.onYouTubeIframeAPIReady = function () {
+        $(function () {
+            $('#video-modal > a').click(function (e) {
+                e.preventDefault();
+                $('#video-modal').addClass('hidden');
+                if (typeof window.myGaugePlayer == 'object' && typeof window.myGaugePlayer.stopVideo == 'function') {
+                    window.myGaugePlayer.pauseVideo()
+                }
+            });
+            $('#neve-play').find('a').click(function (e) {
+                e.preventDefault();
+                $('#video-modal').removeClass('hidden');
+                if (typeof window.myGaugePlayer == 'object' && typeof window.myGaugePlayer.playVideo == 'function') {
+                    window.myGaugePlayer.playVideo()
+                } else {
+                    window.myGaugePlayer = new YT.Player('youtube-player', {
+                        height: '100%',
+                        width: '100%',
+                        playerVars: {
+                            modestbranding: 1,
+                            rel: 0,
+                            color: 'white'
+                        },
+                        videoId: 'XhVV5lUtlbc',
+                        events: {
+                            'onStateChange': function (e) {
+                                Gauge.onPlayerStateChange(e);
+                                var thisPlayer = e.target;
+                                var playerState = thisPlayer.getPlayerState();
+                                if (playerState === YT.PlayerState.ENDED) {
+                                    $('#video-modal').addClass('hidden');
                                 }
+                            },
+                            onReady: function (e) {
+                                e.target.playVideo();
+                                var video_data = e.target.getVideoData();
+                                var label = video_data.video_id + ':' + video_data.title;
+                                dataLayer.push({
+                                    event: 'gauge.youtube',
+                                    action: 'start',
+                                    label: label
+                                })
                             }
-                        });
-                    }
-                });
-
-
+                        }
+                    });
+                }
             });
 
-        }
+
+        });
+
+    }
 
 }).call(this);
